591a4f22d83991169fc2a303d95c2a31b1c9c2af
Somya <somyasanu2805@gmail.com>
2021-12-24 17:36:40 +0530

    modified comparator

--- src/Heap/K_closest_numbers_to_a_target.java
+++ src/Heap/K_closest_numbers_to_a_target.java
@@ -8,7 +8,13 @@ public class K_closest_numbers_to_a_target {
     public static void main(String[] args) {
         int[] arr = {3,4,7,8,9,1,0,2,11};
         int k = 3, target = 5;
-        PriorityQueue<Pair> queue = new PriorityQueue<Pair>(new comp());
+        Comparator<Pair> comp = new Comparator<Pair>() {
+            @Override
+            public int compare(Pair o1, Pair o2) {
+                return o2.key- o1.key;
+            }
+        };
+        PriorityQueue<Pair> queue = new PriorityQueue<Pair>(comp);
         for(int i = 0; i<arr.length; i++){
             queue.add(new Pair(Math.abs(arr[i]-target),i));
             if(queue.size() > k) queue.poll();
@@ -16,13 +22,6 @@ public class K_closest_numbers_to_a_target {
         while(queue.size() > 0){
             System.out.println(arr[queue.poll().value]);
         }
-    }
-}
-class comp implements Comparator<Pair>{
-
-    @Override
-    public int compare(Pair o1, Pair o2) {
-        return o2.key- o1.key;
     }
 }
 class Pair{
